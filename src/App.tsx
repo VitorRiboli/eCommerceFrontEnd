f184cb3ceb5083ad2aa3c7d7ae8c489abd6004d1
vitorriboli <vitorriboli1@gmail.com>
2023-01-18 20:01:52 -0300

    Not Found Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Catalog from "./routes/ClientHome/Catalog";
 import ClientHome from "./routes/ClientHome";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
@@ -12,10 +12,13 @@ function App() {
           <Route index element={<Catalog />} />
           <Route path="catalog" element={<Catalog />} />
           <Route path="product-details/:productId" element={<ProductDetails />} />{/*Rota aninhada*/}
+          
         </Route> {/*Rota 1*/}
 
 
-        {/*Rota 2*/}
+        {/*Rota 2*/}  
+
+        <Route path="*" element={<Navigate to={"/"} />}/>{/*Se não encontrar a rota, cai em um redirecionamento*/}
       </Routes>
     </BrowserRouter>
   );
--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -7,38 +7,36 @@ import { Link } from "react-router-dom";
 
 export default function HeaderClient() {
   return (
-    <>
-      <header className="ec-header-client">
-        <nav className="ec-container">
-          <Link to={"/"} style={{ textDecoration: "none" }}>
-            <h1 className="header-title">Top Shop</h1>
-          </Link>
+    <header className="ec-header-client">
+      <nav className="ec-container">
+        <Link to={"/"} style={{ textDecoration: "none" }}>
+          <h1 className="header-title">Top Shop</h1>
+        </Link>
 
-          <div className="ec-nav-bar-rigth">
-            <div className="ec-menu-items-container">
-              <div className="ec-menu-item">
-                <Link to={"/cart"}>
-                  <img src={cartIcon} alt="Carrinho"></img>
-                </Link>
-              </div>
-              <div className="ec-menu-item">
-                <Link to={"/myaccount"}>
-                  <IconUser />
-                </Link>
-              </div>
+        <div className="ec-nav-bar-rigth">
+          <div className="ec-menu-items-container">
+            <div className="ec-menu-item">
+              <Link to={"/cart"}>
+                <img src={cartIcon} alt="Carrinho"></img>
+              </Link>
             </div>
-
-            <div className="ec-login">
-              <h2>
-                Faça <Link to={"/login"}>LOGIN</Link>
-              </h2>
-              <h2>
-                ou crie seu <Link to={"/cadastro"}>CADASTRO</Link>
-              </h2>
+            <div className="ec-menu-item">
+              <Link to={"/myaccount"}>
+                <IconUser />
+              </Link>
             </div>
           </div>
-        </nav>
-      </header>
-    </>
+
+          <div className="ec-login">
+            <h2>
+              Faça <Link to={"/login"}>LOGIN</Link>
+            </h2>
+            <h2>
+              ou crie seu <Link to={"/cadastro"}>CADASTRO</Link>
+            </h2>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
