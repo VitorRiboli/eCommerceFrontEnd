8adb252cb954d2c9ae7bc0ec610fc02e7b061a77
vitorriboli <vitorriboli1@gmail.com>
2023-01-22 07:31:13 -0300

    BackEnd request for Catalog and for ProductDetails, BASE_URL

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -1,30 +1,36 @@
 import "./styles.css";
 
-import HeaderClient from "../../../components/HeaderClient";
+import { useEffect, useState } from "react";
+
 import CatalogCard from "../../../components/CatalogCard";
 import SearchBar from "../../../components/SearchBar";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 
 import { ProductDTO } from "../../../models/product";
 
-import * as productService from '../../../services/product-service';
-
-
+import * as productService from "../../../services/product-service";
 
 export default function Catalog() {
-  return (
-    /* <HeaderClient /> O headerClient já está alocado na rota principal.
-     * Por isso não necessita coloca-lo aqui  */
+  const [products, setProducts] = useState<ProductDTO[]>([]);
 
+  useEffect(() => {
+    productService.findAll().then((res) => {
+      setProducts(res.data.content);
+    });
+  }, []);
+
+  return (
     <main>
       <section id="catalog-section" className="ec-container">
         <SearchBar />
 
         <div className="ec-catalog-cards ec-mb20 ec-mt20">
-          { /*Chamando a função findAll() de product-service.ts 
-            * findAll() é uma função criada no arquivo de productService que retorna uma lista*/
-            productService.findAll()
-            .map(product => <CatalogCard key={product.id} product={product} />)
+          {
+            /*Chamando a função findAll() de product-service.ts
+             * findAll() é uma função criada no arquivo de productService que retorna uma lista*/
+            products.map((product) => (
+              <CatalogCard key={product.id} product={product} />
+            ))
           }
         </div>
 
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,5 +1,7 @@
 import "./styles.css";
 
+import { useEffect, useState } from "react";
+
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -9,12 +11,24 @@ import ProductDetailsCard from "../../../components/ProductDetailsCard";
 
 import * as productService from "../../../services/product-service";
 
+import { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   /*Aqui vai ler os parametros da rota*/
   const params = useParams();
 
-  const product = productService.findById(Number(params.productId));
+  //Declarando ESTADO e Armazenando dentro do componente um product
+  //setProduc é a função dele que é capaz de alterar o product
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    //const productId = Number(params.productId);
+
+    productService.findById(Number(params.productId)).then((res) => {
+      console.log(res);
+      setProduct(res.data);
+    });
+  }, []);
 
   return (
     /*  <HeaderClient /> */
