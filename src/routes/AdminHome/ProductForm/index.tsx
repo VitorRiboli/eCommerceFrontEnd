288374692b9844935716abe634fd8f69c844334a
vitorriboli <vitorriboli1@gmail.com>
2023-02-13 19:19:30 -0300

    Layout page ProductListing and ProductForm

--- src/routes/AdminHome/ProductForm/index.tsx
+++ src/routes/AdminHome/ProductForm/index.tsx
@@ -2,6 +2,62 @@ import "./styles.css";
 
 export default function ProductForm() {
   return (
-    <h1>Form</h1>
+    <main>
+    <section id="product-form-section" className="ec-container">
+      <div className="ec-product-form-container">
+        <form className="ec-card ec-form">
+          <h2>Dados do Produto</h2>
+
+          <div className="ec-form-controls-container">
+
+            <div>
+              <input className="ec-form-control" type="text" placeholder="Nome" />
+             
+            </div>
+
+            <div>
+              <input className="ec-form-control" type="text" placeholder="Preço" />
+              
+            </div>
+
+            <div>
+              <input className="ec-form-control" type="text" placeholder="Imagem" />
+              
+            </div>
+
+            <div>
+              <select className="ec-form-control ec-select" required>
+                <option value="" disabled selected>Categorias</option>
+                <option value="1">Eletrônicos</option>
+                <option value="2">Casa</option>
+                <option value="3">3</option>
+              </select>
+              
+            </div>
+         
+            <div>
+              <textarea className="ec-textarea ec-form-control" placeholder="Descrição"></textarea>
+            </div>
+
+          </div>
+
+          <div className="ec-product-form-btns">
+            <button type="reset" className="ec-btn ec-btn-white">
+              Cancelar
+            </button>
+            <button type="submit" className="ec-btn ec-btn-login">
+              Salvar
+            </button>
+          </div>
+        </form>
+
+
+
+
+
+      </div>
+    </section>
+
+  </main>
   )
 }
--- src/routes/AdminHome/ProductListing/index.tsx
+++ src/routes/AdminHome/ProductListing/index.tsx
@@ -1,7 +1,66 @@
 import "./styles.css";
 
+import IconEdit from "../../../assets/images/edit.svg";
+import IconDelete from "../../../assets/images/delete.svg";
+
 export default function ProductListing() {
   return (
-    <h1>listing</h1>
+    <main>
+    <section id="product-listing-section" className="ec-container">
+
+      <h2 className="ec-section-title ec-mb20">Cadastro de Produtos</h2>
+
+      <div className="ec-btn-container ec-mb20">
+        <a className="ec-btn ec-btn-white" href="">
+          Novo
+        </a>
+      </div>
+
+      <form className="ec-search-bar">
+        <button type="submit">🔎︎</button>
+        <input type="text" placeholder="Nome do Produto" />
+        <button type="reset">🗙</button>
+      </form>
+
+      <table className="ec-table ec-mb20 ec-mt20">
+        <thead>
+          <th className="ec-tb576">id</th>
+          <th></th>
+          <th className="ec-tb768">Preço</th>
+          <th className="ec-txt-left">Nome</th>
+          <th></th>
+          <th></th>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="ec-tb576">341</td>
+            <td><img className="ec-product-listing-img" src="img/computer.png" alt="Computer" /></td>
+            <td className="ec-tb768">R$ 5000,00</td>
+            <td className="ec-txt-left">Computador Gamer XT Plus Ultra</td>
+            <td><img className="ec-product-listing-btn" src={IconEdit} alt="Editar" /></td>
+            <td><img className="ec-product-listing-btn" src={IconDelete}  alt="Deletar" /></td>
+          </tr>
+
+          <tr>
+            <td className="ec-tb576">341</td>
+            <td><img className="ec-product-listing-img" src="img/computer.png" alt="Computer" /></td>
+            <td className="ec-tb768">R$ 5000,00</td>
+            <td className="ec-txt-left">Computador Gamer XT Plus Ultra</td>
+            <td><img className="ec-product-listing-btn" src={IconEdit}  alt="Editar" /></td>
+            <td><img className="ec-product-listing-btn" src={IconDelete}  alt="Deletar" /></td>
+          </tr>
+
+          
+        </tbody>
+
+      </table>
+    
+
+      <div className="ec-btn-next-page">
+        carregar mais
+      </div>
+      
+    </section>
+  </main>
   )
 }
