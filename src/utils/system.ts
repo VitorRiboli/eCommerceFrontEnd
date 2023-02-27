4fcc98c622ab51e4b717213f25a70240e2aa0f72
vitorriboli <vitorriboli1@gmail.com>
2023-02-27 15:04:24 -0300

    Deploy ready

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,10 +1,9 @@
-export const BASE_URL = "http://localhost:8080"; 
-//export const BASE_URL = "http://localhost:8090";
+//export const BASE_URL = "http://localhost:8090"; //Configurado no Spring a porta 8090
+
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8090";
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
 
 export const CART_KEY = "com.ecommerce.vitor/Cart";
-
 export const TOKEN_KEY = "com.ecommerce.vitor/Token";
 
-export const CLIENT_ID = "myclientid";
-
-export const CLIENT_SECRET = "myclientsecret";
